2afb6f242ef8d4d2db8bd561942bd01c5f498d91
Matthew <mattwills00@gmail.com>
2025-01-28 22:13:57 +0000

    ideassh: line 1: wq: command not found

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import { TwitterApi } from 'twitter-api-v2';
 import dotenv from 'dotenv';
-import axios from 'axios';
-
+import fs from 'fs'; // To read the file
 dotenv.config();
 
+// Function to select a random tweet from the JSON file
+const getRandomTweet = () => {
+    // Read the tweets from the JSON file
+    const tweets = JSON.parse(fs.readFileSync('tweets.json', 'utf-8'));
+
+    const randomIndex = Math.floor(Math.random() * tweets.length);
+    return tweets[randomIndex];
+};
+
+// Function to send the tweet
 const handleTweet = () => {
     const twitterClient = new TwitterApi({
         appKey: process.env.CONSUMER_KEY ?? '',
@@ -14,7 +23,8 @@ const handleTweet = () => {
 
     const tweetClient = twitterClient.readWrite;
 
-    tweetClient.v2.tweet('Milliseconds since 01/01/1970: ' + Date.now());
+    // Get a random tweet and send it
+    tweetClient.v2.tweet(getRandomTweet());
 };
 
 handleTweet();
